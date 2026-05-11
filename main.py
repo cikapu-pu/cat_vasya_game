678483f5788597ee9b1d8aa4685f30d8b4b9397e
Sofia Tyutyulina <sofatutulina@gmail.com>
2026-05-12 01:45:19 +0300

    Добавлен модуль 3: реализована система заклинаний и маны

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 from abc import ABC, abstractmethod
 
 
+class Spell(ABC):
+    def __init__(self, name, damage, mana_cost):
+        self.name = name
+        self.damage = damage
+        self.mana_cost = mana_cost
+
+    @abstractmethod
+    def cast(self):
+        pass
+
+
+class Fireball(Spell):
+    def __init__(self):
+        super().__init__("Fireball", 35, 15)
+
+    def cast(self):
+        return self.damage
+    
+
+class IceLance(Spell):
+    def __init__(self):
+        super().__init__("Ice Lance", 25, 10)
+
+    def cast(self):
+        return self.damage
+    
+
+class LightningBolt(Spell):
+    def __init__(self):
+        super().__init__("Lightning Bolt", 40, 20)
+
+    def cast(self):
+        return self.damage
+
+
 class Unit(ABC):
     def __init__(self, strength, dexterity, constitution, wisdom, intelligence, charisma):
         self.strength = strength            # сила
@@ -9,6 +44,9 @@ class Unit(ABC):
         self.wisdom = wisdom                # мудрость
         self.intelligence = intelligence    # интеллект
         self.charisma = charisma            # харизма
+
+        self.spells = []
+        self.mana = 0
 
     @abstractmethod
     def calculate_max_health(self):
@@ -21,6 +59,19 @@ class Unit(ABC):
     @abstractmethod
     def calculate_defense(self):
         pass
+
+    def add_spell(self, spell):
+        self.spells.append(spell)
+
+    def cast_spell(self, index):
+        spell = self.spells[index]
+
+        if self.mana < spell.mana_cost:
+            raise ValueError("Недостаточно маны для использования заклинания.")
+        
+        self.mana -= spell.mana_cost
+
+        return spell.cast()
 
 
 class Character(Unit):
@@ -35,6 +86,9 @@ class Character(Unit):
         self.max_health = self.calculate_max_health()
         self.damage = self.calculate_damage()
         self.defense = self.calculate_defense()
+
+        self.max_mana = self.calculate_max_mana()
+        self.mana = self.max_mana
 
     def calculate_max_health(self):
         return int(self.constitution * 10 + self.strength / 2)
@@ -58,3 +112,13 @@ class Character(Unit):
         
         elif self.character_class == "hunter":
             return int(self.dexterity * 1.6 + self.constitution / 5)
+        
+    def calculate_max_mana(self):
+        if self.character_class == "warrior":
+            return int(self.intelligence + self.strength / 2)
+        
+        elif self.character_class == "mage":
+            return int(self.intelligence * 3 + self.wisdom)
+        
+        elif self.character_class == "hunter":
+            return int(self.dexterity * 1.5 + self.wisdom / 2)
